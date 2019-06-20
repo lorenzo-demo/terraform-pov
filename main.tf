a3997ab1e9fb1a3543a3bad9aa5e428d0fbfc5bf
lorenzo-demo <49476347+lorenzo-demo@users.noreply.github.com>
2019-06-20 16:51:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
-module "random" {
-    source = "./modules/random"
-    toto_length = 16
-    titi_length = 24
+module "id" {
+  source  = "app.terraform.io/TGITS_ITF_CCE_POC/id/random"
+  version = "0.1.0"
+  toto_length = 16
+  titi_length = 24
 }
 
 output "module_random_toto" {
